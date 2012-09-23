c0fffb032c2c9ee47fbd5ca1bcf918e2809e7a17
alliannas <alliannas@gmail.com>
2012-09-23 19:58:20 +0000

    Commit before merge Added - Receiver to receive incoming calls - Layouts for caller info screen to appear when receiving calls

---
+++ src/com/missingstatement/yeahapp/GetIncomingCallReceiver.java
@@ -0,0 +1,11 @@
+package com.missingstatement.yeahapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexander
+ * Date: 9/23/12
+ * Time: 3:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GetIncomingCallReceiver {
+}
