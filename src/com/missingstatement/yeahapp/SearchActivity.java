00b614da6e4707e1a6a8699ea8deff6d6d860179
alliannas <alliannas@gmail.com>
2012-10-14 21:29:14 +0000

    New look added Possible to call and sms to a number Forgot some xml files

--- src/com/missingstatement/yeahapp/SearchActivity.java
+++ src/com/missingstatement/yeahapp/SearchActivity.java
@@ -1,6 +1,10 @@
 package com.missingstatement.yeahapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -15,13 +19,12 @@ import com.missingstatement.yeahapp.utils.Utils;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class SearchActivity extends Activity
-{
-	private EditText mSearchQueryEdit;
-	private Button mBtnSearch;
+public class SearchActivity extends Activity {
+    private EditText mSearchQueryEdit;
+    private ImageButton mBtnSearch;
 
-    private ListView mResultList;
-    private SearchResultAdapter mSearchResultAdapter;
+    private ExpandableListView mResultList;
+    private SearchResultExpendableAdapter mSearchResultAdapter;
     private ArrayList<HashMap<String, ArrayList<String>>> mSearchResult;
     private ProgressBar mResultListFooterProgress;
 
@@ -30,71 +33,59 @@ public class SearchActivity extends Activity
     private JaSearcher mJaSearcher;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
 
         mUrlsInProgress = new ArrayList<String>();
         mJaSearcher = new JaSearcher();
         mSearchResult = new ArrayList<HashMap<String, ArrayList<String>>>();
-        mSearchResultAdapter = new SearchResultAdapter(mSearchResult);
+        mSearchResultAdapter = new SearchResultExpendableAdapter(mSearchResult);
 
         initViews();
     }
 
-    private void initViews()
-    {
+    private void initViews() {
         mSearchQueryEdit = (EditText) findViewById(R.id.et_searchQuery);
 
-        mBtnSearch = (Button) findViewById(R.id.btn_search);
-        mBtnSearch.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
+        mBtnSearch = (ImageButton) findViewById(R.id.btn_search);
+        mBtnSearch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 search();
             }
         });
 
-        mResultList = (ListView) findViewById(R.id.result_list);
+        mResultList = (ExpandableListView) findViewById(R.id.list_search_results);
         //TODO Add empty view to listview?
 
         View footerView = ((LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.result_list_footer_progress, null);
         mResultListFooterProgress = (ProgressBar) footerView.findViewById(R.id.list_next_progress);
         mResultList.addFooterView(footerView);
         mResultList.setAdapter(mSearchResultAdapter);
-        mResultList.setOnScrollListener(new AbsListView.OnScrollListener()
-        {
+        mResultList.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
-            public void onScrollStateChanged(AbsListView absListView, int i)
-            {
+            public void onScrollStateChanged(AbsListView absListView, int i) {
                 //To change body of implemented methods use File | Settings | File Templates.
             }
 
             @Override
-            public void onScroll(AbsListView lw, final int firstVisibleItem, final int visibleItemCount, final int totalItemCount)
-            {
-                switch(lw.getId())
-                {
-                    case R.id.result_list:
+            public void onScroll(AbsListView lw, final int firstVisibleItem, final int visibleItemCount, final int totalItemCount) {
+                switch (lw.getId()) {
+                    case R.id.list_search_results:
                         final int lastItem = firstVisibleItem + visibleItemCount;
 
-                        if(lastItem != totalItemCount)
-                        {
+                        if (lastItem != totalItemCount) {
                             return;
                         }
 
-                        if(mJaSearcher == null)
-                        {
+                        if (mJaSearcher == null) {
                             return;
                         }
 
-                        if(mJaSearcher.hasNext())
-                        {
+                        if (mJaSearcher.hasNext()) {
 
                             String nextUrl = mJaSearcher.getNextUrl();
-                            if( !mUrlsInProgress.contains(nextUrl) )
-                            {
+                            if (!mUrlsInProgress.contains(nextUrl)) {
                                 //TODO only put nextUrl in mUrlsInProgress if we successfully get next results
                                 mUrlsInProgress.add(nextUrl);
                                 boolean isNextTask = true;
@@ -102,9 +93,7 @@ public class SearchActivity extends Activity
                                         new NextSearchResultHandler(), isNextTask);
                                 nextSearchTask.execute();
                             }
-                        }
-                        else
-                        {
+                        } else {
                             mResultListFooterProgress.setVisibility(View.GONE);
                         }
                 }
@@ -112,17 +101,14 @@ public class SearchActivity extends Activity
         });
     }
 
-    private void search()
-    {
-        if( !Utils.isNetworkOn(this))
-        {
+    private void search() {
+        if (!Utils.isNetworkOn(this)) {
             Utils.showToast(this, getString(R.string.label_network_error));
             return;
         }
 
         String queryText = mSearchQueryEdit.getText().toString();
-        if(TextUtils.isEmpty(queryText))
-        {
+        if (TextUtils.isEmpty(queryText)) {
             Utils.showToast(this, getString(R.string.label_empty_query));
             return;
         }
@@ -137,11 +123,9 @@ public class SearchActivity extends Activity
         searchTask.execute(queryText);
     }
 
-    private class SearchResultHandler implements SearchHandler
-    {
+    private class SearchResultHandler implements SearchHandler {
         @Override
-        public void handleSearchResponse(ArrayList<HashMap<String, ArrayList<String>>> results)
-        {
+        public void handleSearchResponse(ArrayList<HashMap<String, ArrayList<String>>> results) {
             mSearchResult.clear();
             mSearchResult.addAll(results);
             mSearchResultAdapter.notifyDataSetChanged();
@@ -150,80 +134,237 @@ public class SearchActivity extends Activity
         }
     }
 
-    private class NextSearchResultHandler implements SearchHandler
-    {
+    private class NextSearchResultHandler implements SearchHandler {
         @Override
-        public void handleSearchResponse(ArrayList<HashMap<String, ArrayList<String>>> results)
-        {
+        public void handleSearchResponse(ArrayList<HashMap<String, ArrayList<String>>> results) {
             mSearchResult.addAll(results);
             mSearchResultAdapter.notifyDataSetChanged();
         }
     }
 
-    private class SearchResultAdapter extends ArrayAdapter<HashMap<String, ArrayList<String>>>
-    {
+
+    private class SearchResultExpendableAdapter extends BaseExpandableListAdapter {
+        private final int NUM_RESULT_OPTIONS = 3;
         private final int FIRST_ENTRY = 0;
+
+        private final int POSITION_ACTION_CALL = 0;
+        private final int POSITION_ACTION_SEND_SMS = 1;
+        private final int POSITION_ACTION_SAVE_CONTACT = 2;
+
+        private final int DIALOG_CALL = 0;
+        private final int DIALOG_SMS = 1;
+        private final int DIALOG_CONTACT = 2;
+
 
         private ArrayList<HashMap<String, ArrayList<String>>> searchResults;
 
-        public SearchResultAdapter(ArrayList<HashMap<String, ArrayList<String>>> searchResults)
-        {
-            super(SearchActivity.this, R.layout.search_result_item, searchResults);
-
+        public SearchResultExpendableAdapter(ArrayList<HashMap<String, ArrayList<String>>> searchResults) {
             this.searchResults = searchResults;
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent)
-        {
-           View rowView = convertView;
+        public int getGroupCount() {
+            return searchResults.size();
+        }
 
-            if(rowView == null)
-            {
-                rowView = View.inflate(SearchActivity.this, R.layout.search_result_item, null);
+        @Override
+        public int getChildrenCount(int position) {
+            return NUM_RESULT_OPTIONS;
+        }
 
-                ViewHolder viewHolder = new ViewHolder();
-                viewHolder.nameView = (TextView) rowView.findViewById(R.id.txt_list_name);
-                viewHolder.phoneNumberView = (TextView) rowView.findViewById(R.id.txt_list_phoneNumber);
-                viewHolder.addressView = (TextView) rowView.findViewById(R.id.txt_list_address);
+        @Override
+        public Object getGroup(int position) {
+            return null;  //To change body of implemented methods use File | Settings | File Templates.
+        }
 
-                rowView.setTag(viewHolder);
+        @Override
+        public Object getChild(int i, int i1) {
+            return null;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public long getGroupId(int i) {
+            return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public long getChildId(int i, int i1) {
+            return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            View groupView = convertView;
+
+            if (groupView == null) {
+                groupView = View.inflate(SearchActivity.this, R.layout.search_result_item_group, null);
+
+                GroupViewHolder groupViewHolder = new GroupViewHolder();
+                groupViewHolder.nameView = (TextView) groupView.findViewById(R.id.txt_list_name);
+                groupViewHolder.addressView = (TextView) groupView.findViewById(R.id.txt_list_address);
+                groupViewHolder.phoneNumberView = (TextView) groupView.findViewById(R.id.txt_list_phoneNumber);
+                groupViewHolder.indicatorView = (ImageView) groupView.findViewById(R.id.list_indicator);
+
+                groupView.setTag(groupViewHolder);
             }
 
-            HashMap<String, ArrayList<String>> result = searchResults.get(position);
+            HashMap<String, ArrayList<String>> result = searchResults.get(groupPosition);
 
-            if(result == null)
-            {
-                return rowView;
+            if (result == null) {
+                return groupView;
             }
 
             ArrayList<String> phoneNumbers = result.get(Keys.KEY_PHONE_NUMBERS);
             ArrayList<String> names = result.get(Keys.KEY_NAMES);
             ArrayList<String> addresses = result.get(Keys.KEY_ADDRESSES);
 
-            ViewHolder viewHolder = (ViewHolder) rowView.getTag();
+            GroupViewHolder groupViewHolder = (GroupViewHolder) groupView.getTag();
 
-            viewHolder.nameView.setText(names.get(FIRST_ENTRY));
-            viewHolder.phoneNumberView.setText(phoneNumbers.get(FIRST_ENTRY));
+            groupViewHolder.nameView.setText(names.get(FIRST_ENTRY));
+            groupViewHolder.phoneNumberView.setText(phoneNumbers.get(FIRST_ENTRY));
 
-            if(TextUtils.isEmpty(addresses.get(FIRST_ENTRY)))
-            {
-                viewHolder.addressView.setVisibility(View.GONE);
-            }
-            else
-            {
-                viewHolder.addressView.setVisibility(View.VISIBLE);
-                viewHolder.addressView.setText(addresses.get(FIRST_ENTRY));
+            if (TextUtils.isEmpty(addresses.get(FIRST_ENTRY))) {
+                groupViewHolder.addressView.setVisibility(View.GONE);
+            } else {
+                groupViewHolder.addressView.setVisibility(View.VISIBLE);
+                groupViewHolder.addressView.setText(addresses.get(FIRST_ENTRY));
             }
 
-            return rowView;
+            if (isExpanded) {
+                groupView.setBackgroundDrawable(getResources().getDrawable(R.drawable.list_bg_search_detail));
+                groupViewHolder.indicatorView.setImageResource(R.drawable.list_search_indicator_expanded);
+            } else {
+                groupView.setBackgroundDrawable(getResources().getDrawable(android.R.color.transparent));
+                groupViewHolder.indicatorView.setImageResource(R.drawable.list_search_indicator);
+            }
+
+            return groupView;
         }
 
-        private class ViewHolder
-        {
+        @Override
+        public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            View childView = convertView;
+
+            if (childView == null) {
+                childView = View.inflate(SearchActivity.this, R.layout.search_result_item_child, null);
+
+                ChildViewHolder childViewHolder = new ChildViewHolder();
+                childViewHolder.iconView = (ImageView) childView.findViewById(R.id.list_icon);
+                childViewHolder.actionView = (Button) childView.findViewById(R.id.txt_list_action);
+
+                childView.setTag(childViewHolder);
+            }
+
+            ChildViewHolder childViewHolder = (ChildViewHolder) childView.getTag();
+
+            HashMap<String, ArrayList<String>> result = searchResults.get(groupPosition);
+
+            if (result == null) {
+                return childView;
+            }
+
+            final ArrayList<String> phoneNumbers = result.get(Keys.KEY_PHONE_NUMBERS);
+            final ArrayList<String> names = result.get(Keys.KEY_NAMES);
+            final ArrayList<String> addresses = result.get(Keys.KEY_ADDRESSES);
+
+            final String[] phoneNumbersArray = phoneNumbers.toArray(new String[phoneNumbers.size()]);
+
+            //TODO directly call or send sms if person has only 1 number
+
+            //Handle on click depending on action pressed
+            childView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                    switch (childPosition) {
+                        case POSITION_ACTION_CALL:
+
+                            if (phoneNumbers.size() == 1) {
+                                callNumber(phoneNumbers.get(FIRST_ENTRY));
+                            } else {
+                                builder.setTitle(getString(R.string.label_select_number_to_call));
+                                builder.setItems(phoneNumbersArray, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int position) {
+                                        callNumber(phoneNumbers.get(position));
+                                    }
+                                });
+                                builder.create().show();
+                            }
+                            break;
+                        case POSITION_ACTION_SEND_SMS:
+
+                            if (phoneNumbers.size() == 1) {
+                                sendSms(phoneNumbers.get(FIRST_ENTRY));
+                            } else {
+                                builder.setTitle(getString(R.string.label_select_number_to_sms));
+                                builder.setItems(phoneNumbersArray, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int position) {
+                                        sendSms(phoneNumbers.get(position));
+                                    }
+                                });
+                                builder.create().show();
+                            }
+                            break;
+                        case POSITION_ACTION_SAVE_CONTACT:
+                            Utils.showToast(view.getContext(), "Ekki virkt ennþá");
+                    }
+
+                }
+            });
+
+            //Setting the view to the correct one, depending on position
+            switch (childPosition) {
+                case POSITION_ACTION_CALL:
+                    childViewHolder.iconView.setImageResource(R.drawable.list_icon_phone);
+                    childViewHolder.actionView.setText(getString(R.string.action_call));
+                    break;
+                case POSITION_ACTION_SEND_SMS:
+                    childViewHolder.iconView.setImageResource(R.drawable.list_icon_sms);
+                    childViewHolder.actionView.setText(getString(R.string.action_sms));
+                    break;
+                case POSITION_ACTION_SAVE_CONTACT:
+                    childViewHolder.iconView.setImageResource(R.drawable.list_icon_contact);
+                    childViewHolder.actionView.setText(getString(R.string.action_contact));
+                    break;
+            }
+
+            return childView;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        private void callNumber(String phoneNumber) {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + phoneNumber));
+            startActivity(callIntent);
+        }
+
+        private void sendSms(String phoneNumber) {
+            Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+            smsIntent.setData(Uri.parse("sms:" + phoneNumber));
+            startActivity(smsIntent);
+        }
+
+        @Override
+        public boolean isChildSelectable(int i, int i1) {
+            return true;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        private class GroupViewHolder {
             TextView nameView;
             TextView phoneNumberView;
             TextView addressView;
+            ImageView indicatorView;
+        }
+
+        private class ChildViewHolder {
+            ImageView iconView;
+            Button actionView;
         }
     }
 }
